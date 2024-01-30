2b98ccfd7eab4c1c3837fccbebed938bf95fc692
luubang201 <dangvuluu201@gmail.com>
2024-01-30 15:34:32 +0700

    update

--- demo/controller/AddStudentController.js
+++ demo/controller/AddStudentController.js
@@ -1,53 +1,50 @@
 window.AddStudentController = function($scope, $http, $location){
     $scope.title = "Thêm thông tin sinh viên";
-    $scope.addStudent = function (){
-
-        // Call API
+    $scope.addStudent = function () {
+        // Link API
         const apiStudents = 'http://localhost:3000/student';
 
-        // tạo 1 biến để kiểm tra
+        // Tạo 1 biến để kiểm tra
         let flag = true;
-        // kiểm tra từng dữ liệu
+        // Kiểm tra từng trường dữ liệu
         $scope.kiemTra = {
             name: false,
             year: false,
-            course: false
+            couse: false
         }
-
-        // kiểm tra dữ liệu
-        if(!$scope.student || !$scope.student.name){
-            $flag = false;
+         // Kiểm tra dữ liệu
+         if (!$scope.student || !$scope.student.name) {
+            flag = false;
             $scope.kiemTra.name = true;
         }
-
-        if(!$scope.student || !$scope.student.year){
-            $flag = false;
+        if (!$scope.student || !$scope.student.year) {
+            flag = false;
             $scope.kiemTra.year = true;
         }
-
-        if(!$scope.student || !$scope.student.course){
-            $flag = false;
-            $scope.kiemTra.course = true;
+        if (!$scope.student || !$scope.student.couse) {
+            flag = false;
+            $scope.kiemTra.couse = true;
         }
+        if (flag) {
 
-        if(flag){
-        //Dữ liệu nhập từ input
-        let newStuden = {
-            ten: $scope.student.name,
-            namsinh: $scope.student.year,
-            chuyennganh: $scope.student.course
-        }
-        //console.log(newStuden)
-        $http.post(
-            apiStudents,
-            newStuden
-        ).then(function(response){
-            if (response.status == 201){
-                $location.path("trang-chu");
+
+            // Dữ liệu nhập từ input
+            let newStudent = {
+                ten: $scope.student.name,
+                namsinh: $scope.student.year,
+                chuyennganh: $scope.student.cause
             }
-        })
+            //console.log(newStudent);
+            $http.post(
+                apiStudents,
+                newStudent
+            ).then(function (response){
+                if (response.status = 201){
+                    $location.path("trang-chu");
+                }
+            })
         } else {
-            alert("Bạn cần nhập đầy đủ thông tin")
+            alert("Bạn cần nhập đầy đủ thông tin !")
         }
 
     }
